7d4bbf79c9de96812e4ddd56ad4c0e9d0dd9a94a
Joakim Hulthe <joakim@hulthe.net>
2023-11-30 22:56:25 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![feature(test)]
+#![feature(iter_array_chunks, array_chunks, array_windows)]
+#![feature(iter_advance_by)]
+#![feature(binary_heap_drain_sorted)]
+extern crate test;
+
+mod util;
+
+aoc_macro::generate_days!(2023);
---
+++ src/util.rs
@@ -0,0 +1,25 @@
+//! Random utility functions that might be useful for thing
+#![allow(dead_code)]
+
+use std::hash::BuildHasherDefault;
+
+use hashers::fx_hash::FxHasher;
+
+pub type HashMap<K, V> = std::collections::HashMap<K, V, BuildHasherDefault<FxHasher>>;
+pub type HashSet<T> = std::collections::HashSet<T, BuildHasherDefault<FxHasher>>;
+
+/// Return mutable references to two different elements in a slice
+pub fn get_2_mut<T>(slice: &mut [T], first: usize, second: usize) -> [&mut T; 2] {
+    assert!(first < slice.len());
+    assert!(second < slice.len());
+    assert!(first != second);
+
+    let slice = slice.as_mut_ptr();
+    // SAFETY: first and second are within bounds, and do not refer to the same element
+    unsafe {
+        [
+            slice.add(first).as_mut().unwrap(),
+            slice.add(second).as_mut().unwrap(),
+        ]
+    }
+}
